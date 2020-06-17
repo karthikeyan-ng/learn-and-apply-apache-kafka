06efbf068e8e858085cb4713214665ca7e4031fa
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-06-18 00:22:45 +0530

    How to send a Header information along with your message? This is possible when you use ProducerRecord. To create a Header use RecordHeader

--- library-events-producer/src/main/java/com/techstack/kafka/producer/LibraryEventProducer.java
+++ library-events-producer/src/main/java/com/techstack/kafka/producer/LibraryEventProducer.java
@@ -6,12 +6,15 @@ import com.techstack.kafka.domain.LibraryEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -109,7 +112,10 @@ public class LibraryEventProducer {
     }
 
     private ProducerRecord<Integer, String> buildProducerRecord(Integer key, String value, String topic) {
-        return new ProducerRecord<>(topic, null, key, value, null);
+
+        List<Header> recordHeaders = List.of(new RecordHeader("event-source", "scanner".getBytes()));
+
+        return new ProducerRecord<>(topic, null, key, value, recordHeaders);
     }
 
     public SendResult<Integer, String> sendLibraryEventSynchronous(final LibraryEvent libraryEvent)
