b4aab14388c267e04adb8abaf520bf96e67bbf15
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-06-17 23:09:42 +0530

    Use the LibraryEventProducer in Controller to send message.

--- library-events-producer/src/main/java/com/techstack/kafka/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/techstack/kafka/controller/LibraryEventsController.java
@@ -1,20 +1,25 @@
 package com.techstack.kafka.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.techstack.kafka.domain.LibraryEvent;
+import com.techstack.kafka.producer.LibraryEventProducer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class LibraryEventsController {
 
+    private final LibraryEventProducer libraryEventProducer;
+
     @PostMapping("/v1/library-event")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
 
-        // Invoke Kafka producer - later
-
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
