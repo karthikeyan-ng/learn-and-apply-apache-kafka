773cdee0218d419b2ea766cdf288cb1d5e166e94
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-06-18 22:56:10 +0530

    Added ControllerAdvice to validate the Field validation message in the response. The same has been tested through unit test case.

--- library-events-producer/src/test/java/com/techstack/kafka/unit/LibraryEventControllerUnitTest.java
+++ library-events-producer/src/test/java/com/techstack/kafka/unit/LibraryEventControllerUnitTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -56,5 +57,31 @@ public class LibraryEventControllerUnitTest {
                 .content(json)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("Test Post Library Event to verify 4xx")
+    void postLibraryEvent_4xx() throws Exception {
+        //Given
+        Book book = Book.builder().bookId(null).bookAuthor(null).bookName("TDD").build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+
+        String json = objectMapper.writeValueAsString(libraryEvent);
+
+        //When
+        doNothing().when(libraryEventProducer).sendLibraryEvent_Approach2(isA(LibraryEvent.class));
+
+        //Then
+        String expectedErrorMessage = "book.bookAuthor - must not be blank, book.bookId - must not be null";
+
+        mockMvc.perform(post("/v1/library-event")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().string(expectedErrorMessage));
     }
 }
