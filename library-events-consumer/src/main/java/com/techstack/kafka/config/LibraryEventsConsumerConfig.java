67d74dd9e6d43116a361b7225726339c95079071
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-06-19 13:53:27 +0530

    Added application.yml added some configuration and @EnableKafka

---
+++ library-events-consumer/src/main/java/com/techstack/kafka/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.techstack.kafka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class LibraryEventsConsumerConfig {
+}
