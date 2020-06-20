14ed3bd9e343850aa821941ba0e9c9ed82ac599e
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-06-20 15:29:19 +0530

    Added Integration Test for LibraryEventConsumer

---
+++ library-events-consumer/src/test/java/com/techstack/kafka/intg/consumer/LibraryEventsConsumerIntegrationTest.java
@@ -0,0 +1,199 @@
+package com.techstack.kafka.intg.consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.techstack.kafka.consumer.LibraryEventsConsumer;
+import com.techstack.kafka.entity.Book;
+import com.techstack.kafka.entity.LibraryEvent;
+import com.techstack.kafka.entity.LibraryEventType;
+import com.techstack.kafka.repository.LibraryEventsRepository;
+import com.techstack.kafka.service.LibraryEventsService;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+/**
+ * During IntegrationTest if your test don't want to call the actual Kafka broker, then
+ * you can use @EmbeddedKafka with other attributes
+ */
+@EmbeddedKafka(topics = {"library-events"}, partitions = 3)
+
+/**
+ * If you refer {@link org.springframework.kafka.test.EmbeddedKafkaBroker} class which contains
+ * a property called "spring.embedded.kafka.brokers" to run Kafka broker in a test environment.
+ * Inorder to use that server configuration, we have to bind that property value to our
+ * "spring.kafka.producer.bootstrap-servers".
+ *
+ * Similarly, we have to map "spring.kafka.admin.properties.bootstrap.servers" from
+ * "spring.embedded.kafka.brokers"
+ */
+@TestPropertySource(properties = {
+        "spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.consumer.bootstrap-servers=${spring.embedded.kafka.brokers}"
+})
+public class LibraryEventsConsumerIntegrationTest {
+
+    @Autowired
+    EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    KafkaTemplate<Integer, String> kafkaTemplate;
+
+    /**
+     * This KafkaListenerEndpointRegistry will hold all the Listener Containers
+     *
+     * In our case: it's a LibraryEventConsumer
+     */
+    @Autowired
+    KafkaListenerEndpointRegistry endpointRegistry;
+
+    @SpyBean //==> SpyBean will give access to the real bean
+    LibraryEventsConsumer libraryEventsConsumerSpy;
+
+    @SpyBean
+    LibraryEventsService libraryEventsServiceSpy;
+
+    @Autowired
+    LibraryEventsRepository libraryEventsRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+
+        for (MessageListenerContainer messageListenerContainer : endpointRegistry.getListenerContainers()) {
+            /**
+             * What this call is going to do is?
+             * It's going to make sure the container that we have which is LibraryEventConsumer
+             * is going to wait until all the partitions are assigned to work.
+             *
+             * So that our testcase that we are going to write won't have any issues reading
+             * those messages from the Kafka topic.
+             *
+             * So this is a setup which is needed in order to write the consumer Integration Test
+             */
+            ContainerTestUtils.waitForAssignment(messageListenerContainer, embeddedKafkaBroker.getPartitionsPerTopic());
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        libraryEventsRepository.deleteAll();
+    }
+
+    @Test
+    void publishNewLibraryEvent() throws ExecutionException, InterruptedException, JsonProcessingException {
+        //given
+        String json = " {\"libraryEventId\":null,\"libraryEventType\":\"NEW\",\"book\":{\"bookId\":456,\"bookName\":\"Kafka Using Spring Boot\",\"bookAuthor\":\"Dilip\"}}";
+
+        /**
+         * Here this would send message to default topic which is configured in the application.yml
+         *
+         * sendDefault call is Async call.
+         */
+        kafkaTemplate.sendDefault(json).get();
+
+        //when
+        /**
+         * Hence Consumer will run in a different thread from the actual application.
+         * So, after this call it might take a while for the consumer to read that
+         * record and then process the record.
+         *
+         * Basically the processing here involves persisting the record into the
+         * in memory database.
+         *
+         * For this purpose, we have introduced CountDownLatch.
+         * {@link CountDownLatch} is something handy which is going to help us block the current
+         * execution of the thread.
+         * It's really handy when you are writing test cases which involves Async code
+         * execution.
+         */
+        CountDownLatch latch = new CountDownLatch(1);
+
+        /**
+         * This thread will block for 3 second after that it will go to the Then block
+         */
+        latch.await(3, TimeUnit.SECONDS);
+
+        //then
+        /**
+         * Here we are going to verify that "onMessage" method in the {@link LibraryEventsConsumer} class
+         * invoked 1 time or not.
+         */
+        verify(libraryEventsConsumerSpy, times(1)).onMessage(isA(ConsumerRecord.class));
+        verify(libraryEventsServiceSpy, times(1)).processLibraryEvent(isA(ConsumerRecord.class));
+
+        /**
+         * Verify the message has been persisted in the H2 database or not?
+         */
+        List<LibraryEvent> libraryEventList = (List<LibraryEvent>) libraryEventsRepository.findAll();
+        assert libraryEventList.size() == 1;
+        libraryEventList.forEach(libraryEvent -> {
+            assert libraryEvent.getLibraryEventId() != null;
+            assertEquals(456, libraryEvent.getBook().getBookId());
+        });
+
+    }
+
+    @Test
+    void publishUpdateLibraryEvent() throws JsonProcessingException, ExecutionException, InterruptedException {
+        //given
+        /**
+         * Step1: For Update Event, we should have a LibraryEvent record in the database.
+         * Here we are insert a LibraryEvent record.
+         */
+        String json = "{\"libraryEventId\":null,\"libraryEventType\":\"NEW\",\"book\":{\"bookId\":456,\"bookName\":\"Kafka Using Spring Boot\",\"bookAuthor\":\"Dilip\"}}";
+        LibraryEvent libraryEvent = objectMapper.readValue(json, LibraryEvent.class);
+        libraryEvent.getBook().setLibraryEvent(libraryEvent);
+        libraryEventsRepository.save(libraryEvent);
+
+        /**
+         * Step2: publish the update LibraryEvent
+         */
+        Book updatedBook = Book.builder().
+                bookId(456).bookName("Kafka Using Spring Boot 2.x").bookAuthor("Dilip").build();
+        libraryEvent.setLibraryEventType(LibraryEventType.UPDATE);
+        libraryEvent.setBook(updatedBook);
+        String updatedJson = objectMapper.writeValueAsString(libraryEvent);
+
+        /**
+         * Step3: With the updated LibraryEvent, send this message
+         */
+        kafkaTemplate.sendDefault(libraryEvent.getLibraryEventId(), updatedJson).get();
+
+        //when
+        CountDownLatch latch = new CountDownLatch(1);
+        latch.await(3, TimeUnit.SECONDS);
+
+        //then
+        verify(libraryEventsConsumerSpy, times(1)).onMessage(isA(ConsumerRecord.class));
+        verify(libraryEventsServiceSpy, times(1)).processLibraryEvent(isA(ConsumerRecord.class));
+
+        LibraryEvent persistedLibraryEvent = libraryEventsRepository.findById(libraryEvent.getLibraryEventId()).get();
+        assertEquals("Kafka Using Spring Boot 2.x", persistedLibraryEvent.getBook().getBookName());
+    }
+
+}
